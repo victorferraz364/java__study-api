ded66bb777228943afe0ed74d789dd491c82b527
unknown <victorferraz364@gmail.com>
2026-02-13 14:19:15 -0300

    feat: initial project structure with Spring Boot 3

---
+++ src/main/java/com/victordev/studyAPI/StudyApiApplication.java
@@ -0,0 +1,13 @@
+package com.victordev.studyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victordev/studyAPI/StudyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victordev.studyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
