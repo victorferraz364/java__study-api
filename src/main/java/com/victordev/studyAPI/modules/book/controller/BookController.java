ef6af16bc13935f3e0adfd4b6fc0fc352a18a457
unknown <victorferraz364@gmail.com>
2026-02-14 00:54:21 -0300

    :sparkles: feat(books) Implementation of updateBook (put) using ResponseEntity

--- src/main/java/com/victordev/studyAPI/modules/book/controller/BookController.java
+++ src/main/java/com/victordev/studyAPI/modules/book/controller/BookController.java
@@ -3,10 +3,13 @@ package com.victordev.studyAPI.modules.book.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +34,20 @@ public class BookController {
                 .map(book -> ResponseEntity.ok(book))
                 .orElse(ResponseEntity.notFound().build());
     }
+	
+	
+	
+	@PutMapping("/{bookId}")
+	public ResponseEntity<Book> updateBook(@PathVariable UUID bookId, @RequestBody Book bookData) {
+	    return bookRepository.findById(bookId)
+	    		
+	            .map(bookUpdating -> {
+	                BeanUtils.copyProperties(bookData, bookUpdating, "id");
+	                Book bookSaved = bookRepository.save(bookUpdating);
+	                return ResponseEntity.ok(bookSaved);
+	            })
+	            .orElse(ResponseEntity.notFound().build());
+	}
+
 	
 }
