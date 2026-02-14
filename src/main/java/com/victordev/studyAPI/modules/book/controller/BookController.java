aa95e3e649f471d5bc6c10081554cf9122b9e955
unknown <victorferraz364@gmail.com>
2026-02-14 01:09:57 -0300

    :sparkles: feat(books) Implementation of addBook (post) using ResponseEntity

--- src/main/java/com/victordev/studyAPI/modules/book/controller/BookController.java
+++ src/main/java/com/victordev/studyAPI/modules/book/controller/BookController.java
@@ -5,12 +5,16 @@ import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victordev.studyAPI.modules.book.model.Book;
@@ -35,7 +39,13 @@ public class BookController {
                 .orElse(ResponseEntity.notFound().build());
     }
 	
-	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Book> addBook(@RequestBody Book bookData) {
+		Book bookSaved = bookRepository.save(bookData);
+		return ResponseEntity.status(HttpStatus.CREATED).body(bookSaved);
+		
+	}
 	
 	@PutMapping("/{bookId}")
 	public ResponseEntity<Book> updateBook(@PathVariable UUID bookId, @RequestBody Book bookData) {
@@ -49,5 +59,6 @@ public class BookController {
 	            .orElse(ResponseEntity.notFound().build());
 	}
 
+	
 	
 }
